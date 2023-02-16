3771c94b95660a512a31a019c0e15af234ee1a7a
Lakan Santos <rizesuoracle25@gmail.com>
2023-02-16 16:14:12 +0800

    finished update feature

--- src/views/users/EditUser.js
+++ src/views/users/EditUser.js
@@ -1,5 +1,5 @@
 import { Button } from "reactstrap"
-import { useState } from "react"
+import { useState, useCallback, useEffect} from "react"
 const EditUser = ({index, usersData, user, setUserData, setShowEditForm}) => {
     
     const [removeEditForm, setRemoveEditForm] = useState(false)
@@ -7,28 +7,118 @@ const EditUser = ({index, usersData, user, setUserData, setShowEditForm}) => {
     const [emailValue, setemailValue] = useState(user.email)
     const [firstNameValue, setfirstNameValue] = useState(user.firstName)
     const [lastNameValue, setplastNameValue] = useState(user.lastName)
+    const [sw, setSw] = useState(false)
+    const [newValue, setNewValue] = useState([]);
+    const [formValid, setFormValid] = useState(false); 
+
+    const [profileError, setProfileError] = useState('')
+    const [emailError, setEmailError] = useState('')
+    const [firstNameError, setFirstNameError] = useState('')
+    const [lastNameError, setLastNameError] = useState('')
+
+
+
+    const validateForm = useCallback(() => {
+
+        if(profileValue === '') {
+            setProfileError('Profile is required!')
+
+        } else {
+            setProfileError('')
+        }
+
+        if(emailValue === '') {
+            setEmailError('Email is required!')
+        } else if (!/\S+@\S+\.\S+/.test(emailValue)){
+            setEmailError('Invalid email address')
+        } else {
+            setEmailError('')
+        }
+ 
+
+        if(firstNameValue === '') {
+            setFirstNameError('First Name is required!')
+        } else {
+            setFirstNameError('')
+        }
+
+        if(lastNameValue === '') {
+            setLastNameError('Last Name is required!')
+        } else {
+            setLastNameError('')
+        }
+
+
+        setFormValid(firstNameValue && lastNameValue && profileValue && emailValue);
+    },  [profileValue, emailValue, firstNameValue, lastNameValue])
+
+    useEffect(() => {
+        if(!sw)setSw(true)
+        if(sw) validateForm();
+    },[validateForm])
+
+    const handleEdit = () => {
+        
+        let newData = {
+            profile: profileValue,
+            email: emailValue,
+            firstName: firstNameValue,
+            lastName: lastNameValue
+        }
+
+        const editedData = [...usersData]
+        editedData.splice(index, 1, newData)
+        localStorage.setItem('formData', JSON.stringify(editedData))
+        setShowEditForm(false)
+        setUserData(editedData)
+    }
+
+    const handleSave = (e) => {
+        e.preventDefault();
+
+        if(formValid){
+            handleEdit();
+        } else {
+            if(!sw) setSw(true)
+            if(sw) validateForm();
+        }
+
+        
+    }
     return !removeEditForm && (
        <div className="form-container-edit">
             <div className="form-wrapper">
-                <form className="form-edit">
+                <form className="form-edit" onSubmit={handleSave}>
                     <h1 style={{textAlign: 'left', fontWeight: 'bold'}}  className="w-75">Edit</h1>
-                    <div className="w-100 d-flex justify-content-center align-items-center">
+                    <div className="w-100 d-flex flex-column justify-content-center align-items-center">
                         <input type="text" placeholder="Profile" className="w-75 input-edit form-control" value={profileValue} readOnly={false} onChange={e => setprofileValue(e.target.value)} />
+                        {profileError &&
+                        <span className='text-danger w-75'>{profileError}</span>
+                        }
                     </div>
-                    <div className="w-100 d-flex justify-content-center align-items-center">
+                    <div className="w-100 d-flex flex-column justify-content-center align-items-center">
                         <input type="text" placeholder="Email" className="w-75 input-edit form-control"
                         value={emailValue} readOnly={false} onChange={e => setemailValue(e.target.value)} 
                         />
+                        {emailError &&
+                        <span className='text-danger w-75'>{emailError}</span>
+                        }
                     </div>
-                    <div className="w-100 d-flex justify-content-center align-items-center">
+                    <div className="w-100 d-flex flex-column  justify-content-center align-items-center">
                         <input type="text" placeholder="First Name" className="w-75 input-edit form-control"
                         value={firstNameValue} readOnly={false} onChange={e => setfirstNameValue(e.target.value)} 
                         />
+                        {firstNameError &&
+                        <span className='text-danger w-75'>{firstNameError}</span>
+                        }
                     </div>
-                    <div className="w-100 d-flex justify-content-center align-items-center">
+                    <div className="w-100 d-flex flex-column  justify-content-center align-items-center">
                         <input type="text" placeholder="Last Name" className="w-75 input-edit form-control"
                         value={lastNameValue} readOnly={false} onChange={e => setplastNameValue(e.target.value)} 
                         />
+                        {lastNameError &&
+                        <span className='text-danger w-75'>{lastNameError}</span>
+                        }
                     </div>
                     <div className="button-container-edit d-flex gap-2 align-items-start w-75">
                         <Button onClick={() => setShowEditForm(false)}>Cancel</Button>
