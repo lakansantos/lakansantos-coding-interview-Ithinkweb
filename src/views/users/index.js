4f04a5f8c9d0f97bbb635f2db55e3da2eb537bfe
Lakan Santos <rizesuoracle25@gmail.com>
2023-02-16 20:31:20 +0800

    added search feature for table

--- src/views/users/index.js
+++ src/views/users/index.js
@@ -2,12 +2,13 @@ import React, { useCallback, useEffect, useState } from 'react';
 import {Container, Table, Button} from 'reactstrap';
 import AddUser from './AddUser'
 import UserInfo from './UserInfo';
+import  {FcSearch} from 'react-icons/fc'
 
 function Index() {
 
 	const [toggleForm, setToggleForm] = useState(false)
 	const [userData, setUserData] = useState([]);
-
+	const [query, setQuery] = useState('')
 	const getUserData = () => {
 		let data = JSON.parse(localStorage.getItem('formData'))
 		setUserData(data)
@@ -26,12 +27,24 @@ function Index() {
 	}, [])
 
 
-
+	const filteredData = userData.filter(item => {
+		return (
+			item.profile.toLowerCase().includes(query.toLowerCase()) ||
+			item.email.toLowerCase().includes(query.toLowerCase()) ||
+			item.firstName.toLowerCase().includes(query.toLowerCase()) ||
+			item.lastName.toLowerCase().includes(query.toLowerCase())
+		)
+	})
 	return (
 		<Container>
-			<div className='mt-3 text-right'>
+			<div className='mt-3 text-right d-flex justify-content-between align-items-center'>
 				<Button color='primary' onClick={() => setToggleForm(!toggleForm)}>+ Add User</Button>
+				<div className="search-div d-flex justify-content-center align-items-center">
+					<input type="text" placeholder='Search a profile, email...' onChange={(event) => setQuery(event.target.value)}/>
+					<FcSearch style={{fontSize: '25px'}}/>
+				</div>
 			</div>
+
 			
 			{toggleForm && 
 				<AddUser  handleAddUser={handleAddUser} />
@@ -51,7 +64,7 @@ function Index() {
 
 				{userData && userData.length > 0 &&
 				<tbody>
-				{userData.map((user, index) => {
+				{filteredData.map((user, index) => {
 					return user ?(
 						<UserInfo key={index} user={user} index={index} usersData={userData}  setUserData={setUserData}/>
 					)  : null
