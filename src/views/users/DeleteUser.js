f5833a429d7da37610d0225568c9d8f2ebcf06c5
Lakan Santos <rizesuoracle25@gmail.com>
2023-02-18 14:14:02 +0800

    fixed bug for delete user

--- src/views/users/DeleteUser.js
+++ src/views/users/DeleteUser.js
@@ -8,8 +8,10 @@ const DeleteUser = ({index, usersData, setUserData, setShowDeleteModal}) => {
         localStorage.setItem('formData', JSON.stringify(updatedData))
     
         setUserData(updatedData)
+        setShowDeleteModal(false)
     }
 
+    const user = usersData[index] 
 
     return (
         <div className="form-container-item">
@@ -17,15 +19,15 @@ const DeleteUser = ({index, usersData, setUserData, setShowDeleteModal}) => {
                 <div className="d-flex flex-column justify-content-evenly" style={{height: '500px'}}>
                     <h1 style={{textAlign: 'center', fontWeight: 'bold'}}  className="w-100">Delete User</h1>
                     <div className="d-flex flex-row justify-content-between align-items-center border">
-                        {usersData && usersData[index].profile ? (
-                            <img src={usersData[index].profile} alt="Profile" style={{width: '30%', height: '100%', background: 'black'}}/>
+                        {user?.profile  ?(
+                            <img src={user.profile} alt="Profile" style={{width: '30%', height: '100%', background: 'black'}}/>
                         ): (
                             <div style={{height: '100%', width: '50%', objectFit: 'contain'}} className="d-flex flex-column justify-content-center align-items-center bg-black text-white"><img src="/noavatar.png" alt="" />No profile</div>
                         )}
                         <div className="details text-start w-100 ms-2 d-flex flex-column">
-                            <p>First name: {usersData[index].firstName}</p>
-                            <p>Last name: {usersData[index].lastName}</p>
-                            <p>Email: {usersData[index].email}</p>
+                            <p>First name: {user?.firstName}</p>
+                            <p>Last name: {user?.lastName}</p>
+                            <p>Email: {user?.email}</p>
                         </div>
                     </div>
                     <div className="buttonContainer d-flex justify-content-center align-items-center gap-2" style={{height: '100px'}}>
