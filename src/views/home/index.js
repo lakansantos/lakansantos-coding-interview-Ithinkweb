d4604e53c71f1d6360ba0a5da3649ed3e36496ee
Lakan Santos <rizesuoracle25@gmail.com>
2023-02-19 13:34:09 +0800

    changed card details at homepage

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -4,12 +4,28 @@ import {Container, Card} from 'reactstrap';
 function Index() {
 	return (
 		<Container>
-			<Card className='mt-5 p-5'>
-				<h1 className='display-4'>&#123;Lakan Santos&#125;</h1>
+			<Card className='mt-5 p-5' style={{background: '#444', fontFamily: 'sans-serif', color: '#ecf0f1'}}>
+				<img src="Lakan Santos myAvatar.png" alt="my avatar" width={'250px'} height={'250px'}/>
+				<h1 className='display-4' style={{color: '#9DECF9', fontWeight: 'bold'}}>Lakan Santos</h1>
+				<p>
+					Hello, my name is <span style={{fontWeight: 'bold', color: '#9DECF9'}}>Lakan Santos</span> and I am a web developer that specializes in frontend development.
+					I am constantly learning and adapting new information especially in ReactJS. 
+					I am very excited and looking forward to working with you and be part of team's future success.
+					Thank you again for considering me for this interview. 
+				</p>
+				<p  style={{color: '#c5c9ca'}}>You can see my contacts below:</p>
 				<address>
-					<a href='mailto:email@address.com'>lakansantos250@gmail.com</a>
+					<a href='mailto:email@address.com'  style={{color: '#c5c9ca'}}>lakansantos250@gmail.com</a>
 					<br />
-					<a href='tel:+635552368'>(+63) 9659638563</a>
+					<a href='tel:+635552368'  style={{color: '#c5c9ca'}}>(+63) 9659638563</a>
+					<div className="logos-container-main">
+						<a href="https://www.linkedin.com/in/lakan-santos/" target={'_blank'}  rel="noreferrer">
+							<img src="linkedin.png" alt="" width={'50px'} height={'50px'}/>
+						</a>
+						<a href="https://github.com/lakansantos" target={'_blank'}  rel="noreferrer" >
+							<i className="bi bi-github" style={{color: 'white', fontSize: '40px'}} width={'50px'} height={'50px'}></i>
+						</a>
+					</div>
 				</address>
 			</Card>
 		</Container>
